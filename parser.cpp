9f5894060d5e564b46cdaf787463035921d722a4
H-a-y-k <agokugtw@gmail.com>
2023-01-29 19:11:36 +0400

    Removed some commented out lines

--- parser.cpp
+++ parser.cpp
@@ -22,88 +22,6 @@ std::string expand(const std::vector<Token> toks)
 }
 
 
-//std::string match(const Word &word, const std::vector<Token> &tokens, int &current, const std::string &startstr)
-//{
-//    std::string expanded = startstr;
-//    std::string token_expanded = expanded;
-//    std::string tokentext;
-//    int current_start = current;
-
-//    if (current >= tokens.size())
-//    {
-//        return startstr;
-//    }
-//    for (auto symbol = word.begin(); symbol != word.end(); symbol++)
-//    {
-//        if (is_primitive(*symbol))
-//        {
-//            if (expanded+primitive[*symbol] == token_expanded+tokens[current].value)
-//            {
-//                expanded += primitive[*symbol];
-//                token_expanded += tokens.at(current).value;
-//                current++;
-//            }
-//            else
-//            {
-//                current = current_start;
-//                return startstr;
-//            }
-//        }
-//        else if (*symbol == _identifier)
-//        {
-//            auto id = parse_identifier(tokens, current);
-//            if (id.size())
-//            {
-//                expanded += id;
-//                token_expanded += id;
-//                current++;
-//            }
-//            else
-//            {
-//                current = current_start;
-//                return startstr;
-//            }
-//        }
-//        else if (*symbol == _literal)
-//        {
-//            auto lit = parse_literal(tokens, current);
-//            if (lit.size())
-//            {
-//                expanded += lit;
-//                token_expanded += lit;
-//                current++;
-//            }
-//            else
-//            {
-//                current = current_start;
-//                return startstr;
-//            }
-//        }
-//        else
-//        {
-//            auto expanded1 = match(rules[*symbol], std::vector<Token>(tokens.begin(), tokens.end()), current, expanded);
-//            if (expanded != expanded1)
-//            {
-//                expanded = expanded1;
-//                token_expanded = expanded;
-//            }
-//            else
-//            {
-//                current = current_start;
-//                return startstr;
-//            }
-//        }
-//    }
-
-//    std::cout << "/" << expanded << ".." << token_expanded << "/\n";
-//    if (expanded != token_expanded)
-//    {
-//        current = current_start;
-//        return startstr;
-//    }
-//    return expanded;
-//}
-
 std::string parse_primitive(const std::string &value, const std::vector<Token> &tokens, int &current, const std::string &initial_expansion)
 {
     if (current >= tokens.size())
@@ -185,7 +103,7 @@ std::string parse_word(const Word &word, const std::vector<Token> &tokens, int &
         expanded = parsed;
     }
 
-    std::cout << "/" << expanded << "\n"; //token_expanded << "/\n";
+//    std::cout << expanded << "\n";
     return expanded;
 }
 
